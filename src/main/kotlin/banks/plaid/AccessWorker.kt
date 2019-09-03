423f8653f34eba965721959d335e46deeaf082f3
mmarvb8h <44822188+mmarvb8h@users.noreply.github.com>
2019-09-03 13:43:08 -0400

    Allow batch finsync 1 (#2)
    * add 2nd connection port for app req vs web socket req
    
    * first checkin of async stuff. beginning of access_token table create
    
    * new access stuff
    
    * need profile id for access rec creation

--- src/main/kotlin/banks/plaid/AccessWorker.kt
+++ src/main/kotlin/banks/plaid/AccessWorker.kt
@@ -1,6 +1,5 @@
 package bsync.banks.plaid
 
-import kotlinx.serialization.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.*
 
@@ -13,9 +12,11 @@ import bsync.myhttp.*
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import org.koin.core.parameter.parametersOf
+
 import kotlin.coroutines.EmptyCoroutineContext
 
 
+@UseExperimental(kotlinx.serialization.ImplicitReflectionSerializer::class)
 class AccessWorker : KoinComponent {
 
     suspend fun create(clientSession: WsClientSessionI,
@@ -59,7 +60,12 @@ class AccessWorker : KoinComponent {
             val saveExchangeToDb: AccessDb by inject{
                 parametersOf(clientSession) }
             saveExchangeToDb.saveExchangeToken(exchangeToken)
+
             // Send back response
+            @UseExperimental(kotlinx.serialization.UnstableDefault::class)
+            val jsonResponse = Json.stringify(
+                AccessIdMessage.serializer(),
+                AccessIdMessage(accessId = 1))
 
             clientSession.log.debug("in Access Worker end")
         }
--- src/main/kotlin/banks/plaid/PlaidServiceI.kt
+++ src/main/kotlin/banks/plaid/PlaidServiceI.kt
@@ -5,5 +5,10 @@ import bsync.myhttp.WsRequestMessage
 
 
 interface PlaidServiceI {
-    suspend fun access_create(clientSession: WsClientSessionI, publicKey: String)
+  
+    suspend fun access_create(clientSession: WsClientSessionI,
+                              publicAccessKey: String,
+                              finsyncProfileId: Int,
+                              bankName: String?,
+                              bankUid: String?)
 }
